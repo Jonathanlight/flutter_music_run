6098c54be10e3ce9deacb0f5e74ca03b036e7281
clarisse <clarisse@gmail.Com>
2019-02-07 02:31:06 +0100

    screenshot

--- music/lib/main.dart
+++ music/lib/main.dart
@@ -41,7 +41,7 @@ class _MyHomePageState extends State<MyHomePage> {
   AudioPlayer audioPlayer;
   StreamSubscription positionSub;
   StreamSubscription stateSubscription;
-  Duration duree = new Duration(seconds: 10);
+  Duration duree = new Duration(seconds: 0);
   PlayerState statut = PlayerState.stopped;
   int index = 0;
 
@@ -62,93 +62,131 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       backgroundColor: Colors.black,
-      body: Center(
-        child: new Container(
-          margin: const EdgeInsets.all(15.0),
-          padding: const EdgeInsets.all(3.0),
-          decoration: new BoxDecoration(
-            gradient: LinearGradient(
-                    colors: [
-                      Colors.deepOrangeAccent,
-                      Colors.deepPurple,
-                    ],
-                    begin: FractionalOffset(1.2, 0.4),
-                    end: FractionalOffset(-0.3, 0.8),
-                    stops: [0.0, 1.0],
-                  ),
-            border: new Border.all(
-              color: Colors.deepOrange,
-              style: BorderStyle.solid
+      body: new Stack(
+        children: <Widget>[
+          new Container(
+            decoration: new BoxDecoration(
+              image: new DecorationImage(
+                image: new AssetImage("assets/img/background.jpg"), 
+                fit: BoxFit.cover
+              )
             ),
-            boxShadow: <BoxShadow>[
-              BoxShadow(
-                color: Colors.orange,
-                offset: Offset(1.0, 0.9),
-                blurRadius: 20.0,
-              ),
-            ],
-            shape: BoxShape.circle,
           ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              new Card(
-                elevation: 9.0,
-                color: Colors.orange,
-                child: Container(
+          SingleChildScrollView(
+            child: new Container(
+              color: Colors.transparent,
+              margin: EdgeInsets.all(10),
+              child: Center(
+                child: new Container(
+                  margin: const EdgeInsets.all(15.0),
+                  padding: const EdgeInsets.all(3.0),
                   decoration: new BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.deepOrangeAccent,
+                        Colors.deepPurple,
+                      ],
+                      begin: FractionalOffset(1.2, 0.4),
+                      end: FractionalOffset(-0.3, 0.8),
+                      stops: [0.0, 1.0],
+                    ),
+                    border: new Border.all(
+                      color: Colors.deepOrange,
+                      style: BorderStyle.solid
+                    ),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.orange,
+                        offset: Offset(1.0, 0.9),
+                        blurRadius: 20.0,
+                      ),
+                    ],
                     shape: BoxShape.circle,
                   ),
-                  width: MediaQuery.of(context).size.width / 2.5,
-                  child: CircleAvatar(
-                    radius: 100.0,
-                    child: Image.asset(
-                      playlistNow.imagePath,
-                    ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      new Card(
+                        elevation: 9.0,
+                        color: Colors.orange,
+                        child: Container(
+                          decoration: new BoxDecoration(
+                            shape: BoxShape.circle,
+                          ),
+                          width: MediaQuery.of(context).size.width / 2.5,
+                          child: CircleAvatar(
+                            radius: 100.0,
+                            child: Image.asset(
+                              playlistNow.imagePath,
+                            ),
+                          ),
+                        ),
+                      ),
+                      new SizedBox(
+                        height: 50.0,
+                        child: new Center(
+                          child: new Container(
+                            margin: new EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
+                            height: 5.0,
+                            color: Colors.orange,
+                          ),
+                        ),
+                      ),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          textStyleWidget(playlistNow.title, 1.5),
+                        textStyleWidget(playlistNow.artiste, 1.0),
+                        ],
+                      ),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          buttonStyleWidget(Icons.fast_rewind, 30.0, ActionMusic.rewind),
+                          buttonStyleWidget(
+                            (statut == PlayerState.playing) ? Icons.pause : Icons.play_arrow , 100.0
+                            , (statut == PlayerState.playing) ? ActionMusic.pause : ActionMusic.play 
+                            ),
+                          buttonStyleWidget(Icons.fast_forward, 30.0, ActionMusic.forward),
+                        ],
+                      ),
+                      new SizedBox(
+                        height: 100.0,
+                        child: new Center(
+                          child: new Container(
+                            margin: new EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
+                            height: 100.0,
+                            color: Colors.transparent,
+                          ),
+                        ),
+                      ),
+                      new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          textStyleWidget(fromDuration(position), 0.8),
+                          new Slider(
+                            value: position?.inMilliseconds?.toDouble() ?? 0.0,
+                            min: 0.0,
+                            max: duree.inMilliseconds.toDouble(),
+                            inactiveColor: Colors.grey,
+                            activeColor: Colors.orangeAccent,
+                            onChanged: (double d) {
+                              setState(() {
+                                audioPlayer.seek((d / 1000).roundToDouble());
+                              });
+                          }),
+                          textStyleWidget(fromDuration(duree), 0.8),
+                        ],
+                      ),
+                      
+                    ],
                   ),
                 ),
               ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  textStyleWidget(playlistNow.title, 1.5),
-                textStyleWidget(playlistNow.artiste, 1.0),
-                ],
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  buttonStyleWidget(Icons.fast_rewind, 30.0, ActionMusic.rewind),
-                  buttonStyleWidget(
-                    (statut == PlayerState.playing) ? Icons.pause : Icons.play_arrow , 100.0
-                    , (statut == PlayerState.playing) ? ActionMusic.pause : ActionMusic.play 
-                    ),
-                  buttonStyleWidget(Icons.fast_forward, 30.0, ActionMusic.forward),
-                ],
-              ),
-              new Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: <Widget>[
-                  textStyleWidget(fromDuration(position), 0.8),
-                  new Slider(
-                    value: position?.inMilliseconds?.toDouble() ?? 0.0,
-                    min: 0.0,
-                    max: duree.inMilliseconds.toDouble(),
-                    inactiveColor: Colors.grey,
-                    activeColor: Colors.orangeAccent,
-                    onChanged: (double d) {
-                      setState(() {
-                        audioPlayer.seek((d / 1000).roundToDouble());
-                      });
-                  }),
-                  textStyleWidget(fromDuration(duree), 0.8),
-                ],
-              ),
-              
-            ],
-          ),
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+            )
+          )
+        ]
+      )       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 
